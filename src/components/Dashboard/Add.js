719452543a2bed5e59f332a26778e1b861acb436
abelradblanco <abelb200@gmail.com>
2023-10-23 18:15:56 -0600

    added-add-functionality

--- src/components/Dashboard/Add.js
+++ src/components/Dashboard/Add.js
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
 import Swal from 'sweetalert2';
 
-const Add = ({ employees, setEmployees, setIsAdding }) => {
+import { collection, addDoc } from "firebase/firestore";
+import { db } from '../../config/firestore'
+
+const Add = ({ employees, setEmployees, setIsAdding, getEmployees }) => { //pulling it in
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [salary, setSalary] = useState('');
   const [date, setDate] = useState('');
 
-  const handleAdd = e => {
+  const handleAdd = async (e) => {
     e.preventDefault();
 
     if (!firstName || !lastName || !email || !salary || !date) {
@@ -31,9 +34,20 @@ const Add = ({ employees, setEmployees, setIsAdding }) => {
     };
 
     employees.push(newEmployee);
-    localStorage.setItem('employees_data', JSON.stringify(employees));
+
+
+  
+    try{
+      await addDoc(collection(db, "employees"), {
+      ...newEmployee
+     });      
+    }catch(error){
+      console.log(error)
+    }
+
     setEmployees(employees);
     setIsAdding(false);
+    getEmployees()
 
     Swal.fire({
       icon: 'success',
